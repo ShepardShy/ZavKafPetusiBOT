e73dcc5fd069f3f2ffe3e22cdf3d651baf3131e4
Alexandr Farnosov <alexandr.farnosov1@gmail.com>
2022-01-14 15:22:01 +0500

    add sqlite db and README.md

---
+++ database_connector.py
@@ -0,0 +1,59 @@
+import sqlite3
+import logging
+import os
+
+format_log = "%(asctime)s: %(message)s"
+logging.basicConfig(format=format_log, level=logging.INFO,
+                    datefmt="%H:%M:%S")
+
+
+class DbConnector:
+    connection = None
+    cursor = None
+
+    def connect(self):
+        try:
+            path = './db'
+            if not os.path.exists(path):
+                os.mkdir(path)
+            self.connection = sqlite3.connect('./db/apps_base.db')
+            self.cursor = self.connection.cursor()
+            logging.info("Connecting to db")
+        except sqlite3.Error as err:
+            logging.error(f'Error connect to db: {err}')
+
+    def create_table(self):
+        check_table_query = "SELECT name FROM sqlite_master WHERE type='table' AND name='govno_coders';"
+        self.cursor.execute(check_table_query)
+        result_check = self.cursor.fetchone()
+        if result_check is None:
+            create_table_query = """CREATE TABLE govno_coders(
+                                    id INTEGER primary key autoincrement unique,
+	                                user_name TEXT not null unique,
+	                                msg_text TEXT not null;"""
+            self.cursor.execute(create_table_query)
+            self.connection.commit()
+            logging.info("Create table apps on database")
+
+    def start_init(self):
+        self.connect()
+        self.create_table()
+
+    def select_all_coders(self):
+        self.cursor.execute("SELECT * FROM govno_coders;")
+        res = self.cursor.fetchall()
+        return res
+
+    def close_connect(self):
+        if self.connection:
+            self.cursor.close()
+            self.connection.close()
+
+
+def test():
+    conn = DbConnector()
+    conn.start_init()
+
+
+if __name__ == "__main__":
+    test()
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from aiogram import Bot, Dispatcher, executor, types
+from database_connector import DbConnector
 import os
 import logging
+import random
 
 format_log = "%(asctime)s: %(message)s"
 logging.basicConfig(format=format_log, level=logging.INFO,
@@ -13,17 +15,28 @@ if not token:
 bot = Bot(token=token)
 dispatcher = Dispatcher(bot)
 
+db = DbConnector()
+
 
 @dispatcher.message_handler(commands=['start', 'help'])
 async def process_start(message: types.Message):
     response = """Привет! Бот активен!\n
     Доступные команды:\n
-    
+    /govno - возвращает рандомного говно-кодера
     """
     await message.answer(response)
 
 
+@dispatcher.message_handler(commands=['govno'])
+async def random_coder(message: types.Message):
+    all_coders = db.select_all_coders()
+    idx = random.randint(0, len(all_coders) - 1)
+    res = all_coders[idx][2]
+    await message.answer(res)
+
+
 def start_bot():
+    db.start_init()
     executor.start_polling(dispatcher=dispatcher)
 
 
