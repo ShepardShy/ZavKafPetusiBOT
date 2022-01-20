9882714af303badca92c459e8d3b300523a2f25e
ShepardShy <Maybe-It-Is-Me-Mario@yandex.ru>
2022-01-20 13:56:34 +0500

    Дополнение функционала. Была добавлена новая команда ETS2, которая позволяет двум случайным говнокодерам сыграть в русскую рулетку

--- main.py
+++ main.py
@@ -35,6 +35,28 @@ async def random_coder(message: types.Message):
     res = all_coders[idx][2]
     await message.answer(res)
 
+@dispatcher.message_handler(commands=['ETS2'])
+async def russian_roulette(message: types.Message):
+    first_coder = random_coder()
+    second_coder = random_coder()
+    #Проверяем не стреляется ли человек сам с собой
+    if (first_coder == second_coder):
+        while (first_coder == second_coder):
+            second_coder = random_coder()
+    else:
+        await message.answer("Игроки ", first_coder, " и ", second_coder, " решили испытать удачу и запустили евро трак симулятор")
+        #выстрел первого игрока
+        bullet = random.randint(0, 6)
+        if (bullet == 3):
+            answer_fail(first_coder)
+        else:
+            answer_success(first_coder)
+        #выстрел второго игрока
+        bullet = random.randint(0, 6)
+        if (bullet == 3):
+            answer_fail(second_coder)
+        else:
+            answer_success(first_coder)
 
 def start_bot():
     db.start_init()
